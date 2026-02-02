c33b4400f0cb301cf26ab98220100ed2011bdc38
Sören Gade <gade.soeren@gmail.com>
2026-02-03 00:12:55 +0100

    Migrate to Swift 6.2

--- Package.swift
+++ Package.swift
@@ -1,12 +1,11 @@
-// swift-tools-version:5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.2
 
 import PackageDescription
 
 let package = Package(
 	name: "SwiftSerial",
 	platforms: [
-		.macOS(.v10_15),
+		.macOS(.v10_15)
 	],
 	products: [
 		.library(name: "SwiftSerial", targets: ["SwiftSerial"])
